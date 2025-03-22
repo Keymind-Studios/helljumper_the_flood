02e6ae159f2bc43de0e693ab70f87044a5dc0e20
markmcfuzz <magecape10@gmail.com>
2025-03-22 03:33:59 -0600

    rename module

---
+++ lua/modules/helljumper/gameplay_core/playerSprint.lua
@@ -0,0 +1,14 @@
+local engine = Engine
+local balltze = Balltze
+local objectTypes = Engine.tag.objectType
+local tagClasses = Engine.tag.classes
+local getTag = Engine.tag.getTag
+local getObject = Engine.gameState.getObject
+local getPlayer = Engine.gameState.getPlayer
+
+local playerSprint = {}
+
+
+
+
+return playerSprint
