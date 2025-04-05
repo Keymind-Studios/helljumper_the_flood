9639556fc99fc0ee9d2d1bac022e5abb6899620b
markmcfuzz <magecape10@gmail.com>
2025-04-05 04:59:07 -0600

    updated module

--- lua/modules/helljumper/witch_system/witchMaster.lua
+++ lua/modules/helljumper/witch_system/witchMaster.lua
@@ -1,5 +1,12 @@
+local engine = Engine
+local balltze = Balltze
+local objectTypes = Engine.tag.objectType
+local tagClasses = Engine.tag.classes
+local getTag = Engine.tag.getTag
+local getObject = Engine.gameState.getObject
+local getPlayer = Engine.gameState.getPlayer
 
-
--- WITCH modules
+---WITCH modules
 local rucksack = require "helljumper.witch_system.rucksack.rucksack"
 local techTree = require "helljumper.witch_system.tech_tree.techTree"
+local state = require "helljumper.witch_system.stateLoader"
