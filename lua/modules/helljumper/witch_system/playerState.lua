857db8856837a3c1c33bbe6716ddcacd284b1eca
MangoFizz <ccs81.mr@gmail.com>
2025-04-05 16:33:01 -0600

    Destroy player's weapons and create new ones based on the saved state

--- lua/modules/helljumper/witch_system/playerState.lua
+++ lua/modules/helljumper/witch_system/playerState.lua
@@ -9,6 +9,9 @@ local state = require "helljumper.witch_system.stateLoader"
 
 local playerState = {}
 
+---@type BalltzeEventListener
+local tickEventHandle = nil
+
 function playerState.saveHealth()
     local player = getPlayer()
     if player then
@@ -103,33 +106,49 @@ function playerState.loadWeapon()
     if player then
         local biped = getObject(player.objectHandle, objectTypes.biped)
         if biped then
+            -- Delete the existing weapons
             for weaponIndex = 1, 4 do
-                local weapon = getObject(biped.weapons[weaponIndex], objectTypes.weapon)
-                if weapon then
-                    local tag = getTag(weapon.tagHandle, tagClasses.weapon)
-                    if tag then
-                        --local tagPath = tag.path
+                if not biped.weapons[weaponIndex]:isNull() then
+                    engine.gameState.deleteObject(biped.weapons[weaponIndex])
+                end
+            end
+
+            -- Create a tick event to create and add the weapons to the player's inventory
+            tickEventHandle = balltze.event.tick.subscribe(function(ev) 
+                if ev.time == "before" then
+                    for weaponIndex = 1, 4 do
                         local roundsLoadedKey = "player.weaponInventory." .. weaponIndex .. ".weaponTag.roundsLoaded"
                         local roundsUnloadedKey = "player.weaponInventory." .. weaponIndex .. ".weaponTag.roundsUnloaded"
                         local ageKey = "player.weaponInventory." .. weaponIndex .. ".weaponTag.energy"
                         local weaponPathKey = "player.weaponInventory." .. weaponIndex .. ".weaponTag.tagPath."
-                        if state.exists(roundsLoadedKey) and state.exists(roundsUnloadedKey) and
-                        state.exists(ageKey) and state.exists(weaponPathKey) then
+                        
+                        if state.exists(roundsLoadedKey) and state.exists(roundsUnloadedKey) and state.exists(ageKey) and state.exists(weaponPathKey) then
                             local roundsLoadedValue = state.getInteger(roundsLoadedKey)
                             local roundsUnloadedValue = state.getInteger(roundsUnloadedKey)
                             local ageValue = state.getNumber(ageKey)
                             local weaponPathValue = state.getString(weaponPathKey)
                             if roundsLoadedValue and roundsUnloadedValue and ageValue and weaponPathValue then
-                                weapon.magazines[1].roundsLoaded = roundsLoadedValue
-                                weapon.magazines[1].roundsUnloaded = roundsUnloadedValue
-                                weapon.age = ageValue
-                                tag.path = weaponPathValue
-                                --engine.gameState.unitAddWeapon(player.objectHandle.value, tag.handle)
+                                local weaponTag = getTag(weaponPathValue, tagClasses.weapon)
+                                if weaponTag then
+                                    local weaponHandle = engine.gameState.createObject(weaponTag.handle, nil, { x = 0, y = 0, z = 0 })
+                                    local weapon = getObject(weaponHandle, objectTypes.weapon)
+                                    if weapon then
+                                        engine.gameState.unitAddWeaponToInventory(player.objectHandle, weaponHandle, 0)
+                                        weapon.magazines[1].roundsLoaded = roundsLoadedValue
+                                        weapon.magazines[1].roundsUnloaded = roundsUnloadedValue
+                                        weapon.age = ageValue
+                                    else
+                                        error("Failed to create weapon object")
+                                    end
+                                end
                             end
                         end
                     end
+
+                    -- Remove the tick event after creating the weapons so it doesn't run again
+                    tickEventHandle:remove()
                 end
-            end
+            end)
         end
     end
 end
