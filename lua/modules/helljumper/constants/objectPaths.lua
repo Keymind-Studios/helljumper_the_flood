172329b19a1fded1b1c54d0360e041d2c9ae23d2
markmcfuzz <magecape10@gmail.com>
2025-04-09 08:49:45 -0600

    add new module for object paths

---
+++ lua/modules/helljumper/constants/objectPaths.lua
@@ -0,0 +1,31 @@
+local objectPaths = {}
+
+objectPaths.bipeds = {
+    mckay_bipd = "keymind\\helljumper\\objects\\characters\\melissa_mckay\\melissa_mckay" -- Mckay Gridharvolur Biped
+}
+
+objectPaths.collisionGeometry = {
+    grid_coll = "keymind\\helljumper\\objects\\characters\\melissa_mckay\\melissa_mckay" -- Gridharvolur Coll
+}
+
+objectPaths.weapons = {
+    ma38_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\assault_rifle\\assault_rifle_ma38", -- Assault Rifle MA38
+    we38_ex_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\assault_rifle\\assault_rifle_ma38_ex", -- Assault Rifle MA38 High Explosive Rounds
+    vk78_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\vk78_commando\\vk78_commando", -- VK78 Commando
+    br65h_weap = "keymind\\the_flood\\weapons\\rifle\\br65h\\br_65h"
+}
+
+objectPaths.weaponHudInterface = {
+    ma38_hud = "keymind\\helljumper\\objects\\weapons\\rifle\\assault_rifle\\assault_rifle_ma38", -- Assault Rifle MA38 HUD
+    vk78_hud = "keymind\\helljumper\\objects\\weapons\\rifle\\vk78_commando\\vk78_commando", -- VK78 Commando HUD
+    br65h_hud = "keymind\\the_flood\\weapons\\rifle\\br65h\\br_65h"
+}
+
+objectPaths.deviceControl = {
+    ap_726_ammo_150 = "keymind\\helljumper\\objects\\devices\\ammo_crates\\ap_726_ammo_150", -- Armor Piercing Ammo = 150 rounds
+    ex_726_ammo_150 = "keymind\\helljumper\\objects\\devices\\ammo_crates\\ex_726_ammo_150", -- Explosive Ammo = 150 rounds
+    ir_726_ammo_150 = "keymind\\helljumper\\objects\\devices\\ammo_crates\\ir_726_ammo_150", -- Incendiary Ammo = 150 rounds
+    ds_726_ammo_150 = "keymind\\helljumper\\objects\\devices\\ammo_crates\\dr_726_ammo_150" -- Disruption Ammo = 150 rounds
+}
+
+return objectPaths
