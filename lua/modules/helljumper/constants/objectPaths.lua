37cfab8e7423a7fa5a927e8d5c443fcd8cd599ae
markmcfuzz <magecape10@gmail.com>
2025-04-10 03:45:40 -0600

    updated module

--- lua/modules/helljumper/constants/objectPaths.lua
+++ lua/modules/helljumper/constants/objectPaths.lua
@@ -10,15 +10,23 @@ objectPaths.collisionGeometry = {
 
 objectPaths.weapons = {
     ma38_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\assault_rifle\\assault_rifle_ma38", -- Assault Rifle MA38
-    we38_ex_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\assault_rifle\\assault_rifle_ma38_ex", -- Assault Rifle MA38 High Explosive Rounds
+    ma38_ex_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\assault_rifle\\assault_rifle_ma38_ex", -- Assault Rifle MA38 High Explosive Rounds
     vk78_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\vk78_commando\\vk78_commando", -- VK78 Commando
-    br65h_weap = "keymind\\the_flood\\weapons\\rifle\\br65h\\br_65h"
-}
-
-objectPaths.weaponHudInterface = {
-    ma38_hud = "keymind\\helljumper\\objects\\weapons\\rifle\\assault_rifle\\assault_rifle_ma38", -- Assault Rifle MA38 HUD
-    vk78_hud = "keymind\\helljumper\\objects\\weapons\\rifle\\vk78_commando\\vk78_commando", -- VK78 Commando HUD
-    br65h_hud = "keymind\\the_flood\\weapons\\rifle\\br65h\\br_65h"
+    br65h_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\br65h\\br_65h", -- Battle Rifle 65H
+    dmr_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\dmr\\dmr_392",
+    saw_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\lmg_saw\\lmg_saw",
+    m90_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\shotgun_m90\\shotgun_m90",
+    m7_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\smg\\smg_m7",
+    sniper_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\sniper_rifle\\sniper_rifle_srs99c",
+    caster_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\cv_grenade_launcher\\cv_grenade_launcher",
+    storm_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\cv_storm_rifle\\cv_storm_rifle",
+    stalker_weap = "keymind\\helljumper\\objects\\weapons\\rifle\\stalker_rifle\\stalker_rifle",
+    m6s_weap = "keymind\\helljumper\\objects\\weapons\\pistol\\magnum_m6s\\magnum_m6s",
+    pp_weap = "keymind\\helljumper\\objects\\weapons\\pistol\\plasma_pistol\\plasma_pistol",
+    needler_weap = "keymind\\helljumper\\objects\\weapons\\pistol\\needler\\needler_t54c",
+    zapper_weap = "keymind\\helljumper\\objects\\weapons\\pistol\\proto_arc_zapper\\proto_arc_zapper",
+    spnkr_weap = "keymind\\helljumper\\objects\\weapons\\support_high\\spnkr_rocket_launcher\\m41_spknr",
+    skewer_weap = "keymind\\helljumper\\objects\\weapons\\support_high\\skewer\\skewer"
 }
 
 objectPaths.deviceControl = {
