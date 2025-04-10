b3d1d83c70fc30d637883ac61c35b0c721511353
markmcfuzz <magecape10@gmail.com>
2025-04-10 03:49:22 -0600

    added comments, edited error messages

--- lua/modules/helljumper/witch_system/stateLoader.lua
+++ lua/modules/helljumper/witch_system/stateLoader.lua
@@ -1,5 +1,4 @@
 local balltze = Balltze
-local engine = Engine
 local fs = balltze.filesystem
 
 local stateloader = {}
@@ -17,68 +16,76 @@ function stateloader.load()
         configFile = balltze.config.open(configPath .. "\\playerData.json")
         configFile:load()
     else
-        error("Config File Already Loaded")
+        error("Config file desn't exist")
     end
 end
 
+-- Save key | value in a config file
 function stateloader.save()
     if configFile then
         configFile:save()
     end
 end
 
+-- Create a new key | value in the config file
 function stateloader.set(key, value)
     if configFile then
         configFile:set(key, value)
     else
-        error("Failed to Set Values")
+        error("Failed to create key | value")
     end
 end
 
+-- Check if the key exist in the config file
 function stateloader.exists(key)
     if configFile then
         return configFile:exists(key)
     else
-        error("Config File Isn't Loaded")
+        error("Key doesn't exist")
     end
 end
 
+-- Remove an existing key in the config file
 function stateloader.remove(key)
     if configFile then
         configFile:remove(key)
     else
-        error ("Cannot Remove Key")
+        error ("Can't remove, key doesn't exist")
     end
 end
 
+-- Get a string from a value
 function stateloader.getString(key)
     if configFile then
         return configFile:getString(key)
     else
-        error("Failed to get String")
+        error("Value is not a string")
     end
 end
 
+-- Get a number from a value
 function stateloader.getNumber(key)
     if configFile then
         return configFile:getNumber(key)
     else
-        error("Failed to get number")
+        error("Value is not a number")
     end
 end
 
+-- Get an integer from a value
 function stateloader.getInteger(key)
     if not configFile then
-        error("Failed to get Integer")
+        error("Value is not an integer")
     end
     return configFile:getInteger(key)
 end
 
+-- Get a boolean from a value
 function stateloader.getBoolean(key)
     if configFile then
         return configFile:getBoolean(key)
     else
-        error("Failed to get boolean")
+        error("Value is not a boolean")
     end
 end
 
